2de2edf2e0eba52e78dd209718f4acbe0932c7aa
Mattia.Moroder <mattia.moroder@physik.uni-muenchen.de>
2022-10-10 15:15:08 +0200

    added docstrings for spin_one_half_lattice.py

--- evos/src/lattice/spin_one_half_lattice.py
+++ evos/src/lattice/spin_one_half_lattice.py
@@ -6,7 +6,16 @@ class SpinOneHalfLattice():
     """Vacuum = |1 0 ... 0 > = |up up .... up >. sigma plus (sp) is the annihilator and sigma minus (sm) is the creator"""
     
     def __init__(self, n_sites: int):
-        """ """
+        """Saves sigma_x, sigma_y, sigma_z, sigma_plus, sigma_minus as instance variables and adds them to the operators dictionary.
+        All these operator are saved as instance variables, together with the the identity operator and the vacuum state Vacuum = |1 0 ... 0 > = |up up .... up >.
+        sigma plus (sp) is the annihilator and sigma minus (sm) is the creator
+        
+
+        Parameters
+        ----------
+        n_sites : int
+            number of spin 1/2 lattice sites
+        """
         #operators
         sx = np.array([[0,1], [1,0]],dtype='complex')
         sy = np.array([[0,-1j], [1j,0]],dtype='complex')
@@ -29,7 +38,21 @@ class SpinOneHalfLattice():
         self.vacuum_state = vacuum_state
      
     def sso(self, operator_name: str, site: int) -> np.ndarray :
-        """ """
+        """Given an operator name and a site number, it computes the single site operator acting on the whole Hilbert space
+        by computing kronecker products with the identity left and right to the site on which the operator is applied.
+
+        Parameters
+        ----------
+        operator_name : str
+            key of the dictionary instance variable 'operators' computed by the __init__() method
+        site : int
+            lattice site on wich the single site operator acts
+
+        Returns
+        -------
+        np.ndarray
+            single site operator acting on the whole Hilbert space
+        """
         operator = self.operators[operator_name]
         if site == 0:
             single_site_operator = operator.copy()
