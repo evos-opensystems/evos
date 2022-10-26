45415cffbcb52dde43f05942d8a9390c02455c46
Mattia Moroder <ra69tuc@kcs-login.cos.lrz.de>
2022-10-26 10:37:28 +0200

    changed tdvp worker init state

--- evos/src/methods/mps_quantum_jumps.py
+++ evos/src/methods/mps_quantum_jumps.py
@@ -160,7 +160,7 @@ class MPSQuantumJumps():
         self.conf_tdvp.exp_conf.mode = 'N'  #FIXME: specify this before
         self.conf_tdvp.exp_conf.submode = 'a' #FIXME: specify this before
         self.conf_tdvp.exp_conf.minIter = 20 #FIXME: specify this before
-        worker = ptn.mp.tdvp.PTDVP( psi.copy(),[self.H_eff.copy()], self.conf_tdvp.copy() )
+        worker = ptn.mp.tdvp.PTDVP( psi_t.copy(),[self.H_eff.copy()], self.conf_tdvp.copy() )
         
         os.mkdir( str( trajectory ) ) #create directory in which to run trajectory
         os.chdir( str( trajectory ) ) #change to it
