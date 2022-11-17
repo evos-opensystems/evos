b22e5e350f30dbe0da90f59129285e2b033d2f18
Mattia Moroder <ra69tuc@kcs-login.cos.lrz.de>
2022-11-17 11:55:45 +0100

    added spinless_fermions_lattice.py

--- evos/src/methods/mps_quantum_jumps_no_normalization_adaptive_timestep.py
+++ evos/src/methods/mps_quantum_jumps_no_normalization_adaptive_timestep.py
@@ -118,7 +118,7 @@ class MPSQuantumJumps():
         states_after_jump_operator_application_list = []
         for jump_op in self.lindbl_op_list:
             ###threshold_MPS = tdvp_trunc_threshold * state1.norm()  weight_MPS = tdvp_trunc_weight * state1.norm()**2 FIXME: scale the truncation!
-            states_after_jump_operator_application, inutile = ptn.mp.apply_op_fit( psi.copy(), jump_op,  ptn.Truncation(1e-8,2000,2000,1e-10), 1e-8, 12, 4)
+            states_after_jump_operator_application, inutile = ptn.mp.apply_op_fit( psi.copy(), jump_op,  ptn.Truncation(), 1e-8, 12, 4) #ptn.Truncation(1e-8,2000,2000,1e-10)
             ####
             # states_after_jump_operator_application = psi.copy()  #FIXME test wheter with exact application mps and ed agree!
             # ptn.mp.apply_op_naive( states_after_jump_operator_application, jump_op)
