323a4ebb401886b7a7931aff4f2212bbdb28afbb
Reka <Reka.schwengelbeck@physik.uni-muenchen.de>
2023-01-19 13:14:53 +0100

    uploaded all necessary files for creation of phase diagram

--- examples/example_main.sh
+++ examples/example_main.sh
@@ -9,7 +9,7 @@ U=1 #2
 V=1 #3
 
 
-for t in {1..2};
+for t in {1,1.5,2};
 do
 python3 example1.py $t $U $V
 python3 example2.py $t $U $V
@@ -19,7 +19,7 @@ t=1 #1
 U=1 #2
 V=1 #3
 
-for U in {-1..3};
+for U in {-1,-0.5,0,0.5,1,1.5};
 do
 python3 example1.py $t $U $V
 python3 example2.py $t $U $V
