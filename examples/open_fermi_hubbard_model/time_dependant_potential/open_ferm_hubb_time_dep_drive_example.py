13b44d720cc738561adcbb03673529fb0f00c1b2
Reka <Reka.schwengelbeck@physik.uni-muenchen.de>
2023-02-08 11:20:05 +0100

    better example description and better example

--- examples/open_fermi_hubbard_model/time_dependant_potential/open_ferm_hubb_time_dep_drive_example.py
+++ examples/open_fermi_hubbard_model/time_dependant_potential/open_ferm_hubb_time_dep_drive_example.py
@@ -13,49 +13,21 @@ import evos.src.lattice.spinful_fermions_lattice as spinful_fermions_lattice
 import evos.src.methods.ed_time_dep_hamiltonian_lindblad_solver_new as solve
 import matplotlib.pyplot as plt
 
-
-def plot_expectation_values(results, ylabels=[], title=None, show_legend=False,
-                            fig=None, axes=None, figsize=(8, 4)):
-
-    if not isinstance(results, list):
-        results = [results]
-
-    n_e_ops = max([len(result.expect) for result in results])
-
-    if not fig or not axes:
-        if not figsize:
-            figsize = (12, 3 * n_e_ops)
-        fig, axes = plt.subplots(n_e_ops, 1, sharex=True,
-                                 figsize=figsize, squeeze=False)
-
-    for r_idx, result in enumerate(results):
-        for e_idx, e in enumerate(result.expect):
-            axes[e_idx, 0].plot(result.times, e,
-                                label="%s [%d]" % (result.solver, e_idx))
-
-    if title:
-        axes[0, 0].set_title(title)
-
-    axes[n_e_ops - 1, 0].set_xlabel("time", fontsize=12)
-    for n in range(n_e_ops):
-        if show_legend:
-            axes[n, 0].legend()
-        if ylabels:
-            axes[n, 0].set_ylabel(ylabels[n], fontsize=12)
-
-    return fig, axes
-
-t_hop = 1
-U = 1
-
-
-alpha = 1
-
 n_sites = 2
 dim_H = 4 ** n_sites
 
-T = 10
-dt = 0.01
+# coupling parameters
+t_hop = 1
+U = 1
+# oscillation frequency
+omega = 0.11
+
+# coupling of lindblad operators
+alpha = 1
+
+# for lindblad solver
+T = 10 # final time
+dt = 0.01 #time step size
 tsteps = int(T/dt)
 t = np.linspace(0,T, tsteps)
 #print(t)
@@ -80,34 +52,32 @@ def H(t):
         n_down = np.dot(spin_lat.sso('adag',k, 'down'), spin_lat.sso('a',k, 'down'))
           
         coul += np.dot(n_up,n_down)
-    H = t_hop*hop + U*coul    
+
+    
+    if t>2:
+        H = -np.cos(omega*t)*t_hop*hop +U*coul
+        
+    if t<2:
+        H = U*coul
    
     return H
 
-H1 = []
-for t11 in t:
-    H1.append(H(t11))
-    
-#print('h1 =', H1)
 
 # alternate spin up down state: first site: up, second site: down, third site: up and so on... 
-def state00_ket(n_sites):
+def vac_ket(n_sites):
             
-    state_ket = np.zeros((dim_H, 1), dtype = 'complex')
+    vac_ket = np.zeros((dim_H, 1), dtype = 'complex')
     for i in range(0,dim_H+1):
         if i == 0:
-            state_ket[i,0] = 1
+            vac_ket[i,0] = 1
     
-    return state_ket
+    return vac_ket
 
 
-updown_ket = state00_ket(n_sites)
-
+updown_ket = vac_ket(n_sites)
 for i in np.arange(2,n_sites+1,2):
-    #updown_ket = np.dot(c_up_dag(i-1, N), updown_ket)
-    updown_ket = np.dot(spin_lat.sso('adag',i-1, 'up'), updown_ket)
     
-    #updown_ket = np.dot(c_down_dag(i, N), updown_ket)
+    updown_ket = np.dot(spin_lat.sso('adag',i-1, 'up'), updown_ket)
     updown_ket = np.dot(spin_lat.sso('adag',i, 'down'), updown_ket)
 
 
@@ -120,23 +90,18 @@ def L(k, N):
     return L
 
 
-#print(L(2,3))
 
 L_list_left = []
 for k in range(0, n_sites):
     L_list_left.append(L(k+1, n_sites))
 
-#print('exp =', H.dot(rho_updown).trace())
-#print('exp_L =', L_list_left[1].dot(rho_updown).trace())
-   
+# observable: number of up spin on site 1
 n_up_1 = np.dot(spin_lat.sso('adag',1, 'up'), spin_lat.sso('a',1, 'up'))
-equation = solve.LindbladEquation(dim_H, H, L_list_left)
-#lindblad = solve.TimeDepHamiltonianLindblad(dim_H, H, dt, T)
-print(equation)
-exp_n , t11  = solve.SolveLindbladEquation(dim_H, H, L_list_left, dt, T).solve(n_up_1, updown_ket)
-#print(sol)
-#print(lindblad.solve_lindblad_equation(updown_ket, H, L_list_left, n_up_1))
 
+
+#equation = solve.LindbladEquation(dim_H, H, L_list_left)
+# solve lindblad eqaution and compute observable
+exp_n , t11  = solve.SolveLindbladEquation(dim_H, H, L_list_left, dt, T).solve(n_up_1, updown_ket)
 
 
 plt.plot(t11, exp_n, label='reka nfup')
@@ -144,3 +109,4 @@ plt.xlabel('t')
 plt.ylabel('$< \hat n >$')
 plt.legend()
 plt.show()
+
