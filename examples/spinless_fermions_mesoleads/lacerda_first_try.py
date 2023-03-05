3bd62eb053894f4208838df2f0c587bfb5256d88
Mattia.Moroder <mattia.moroder@physik.uni-muenchen.de>
2023-03-05 15:34:57 +0100

    making leads and sys ham

--- examples/spinless_fermions_mesoleads/lacerda_first_try.py
+++ examples/spinless_fermions_mesoleads/lacerda_first_try.py
@@ -103,7 +103,7 @@ def fermi_dist(beta, e, mu):
     return f
 
 #MAKE LATTICE
-spin_lat = spinless_fermions_lattice.SpinlessFermionsLattice(n_tot)
+ferm_lat = spinless_fermions_lattice.SpinlessFermionsLattice(n_tot)
 
 #LEADS HAMILTONIAN
 def H_leads_left( eps_vector_l, k_vector_l, mu_L ):
@@ -112,14 +112,14 @@ def H_leads_left( eps_vector_l, k_vector_l, mu_L ):
     h_onsite_l = np.zeros( (dim_tot, dim_tot), dtype = 'complex' )
     for site in range( n_lead_left ): 
         # print( 'on site ', site)
-        h_onsite_l += ( eps_vector_l[site] - mu_L ) * ( spin_lat.sso('ch',site ) @ spin_lat.sso('c', site ) )
+        h_onsite_l += ( eps_vector_l[site] - mu_L ) * ( ferm_lat.sso('ch',site ) @ ferm_lat.sso('c', site ) )
     
     # system-lead coupling
     #every lead site is coupled to the leftmost system site
     h_sys_lead = np.zeros( (dim_tot, dim_tot), dtype = 'complex' )
     for site in range(n_lead_left):
         # print( 'coupling {} and {}'.format( site, n_lead_left ) )
-        h_sys_lead += k_vector_l[site] *  ( spin_lat.sso('ch',site ) @ spin_lat.sso('c', n_lead_left ) + spin_lat.sso('ch',n_lead_left ) @ spin_lat.sso('c', site ) )
+        h_sys_lead += k_vector_l[site] *  ( ferm_lat.sso('ch',site ) @ ferm_lat.sso('c', n_lead_left ) + ferm_lat.sso('ch',n_lead_left ) @ ferm_lat.sso('c', site ) )
     
         
     h_left_lead = h_onsite_l + h_sys_lead    
@@ -132,14 +132,14 @@ def H_leads_right( eps_vector_r, k_vector_r, mu_R ):
     h_onsite_r = np.zeros( (dim_tot, dim_tot), dtype = 'complex' )
     for site in range( n_lead_left + n_system , n_tot ): 
         print( 'on site ', site)
-        h_onsite_r += ( eps_vector_r[ site - n_lead_left - n_system ] - mu_R ) * ( spin_lat.sso('ch',site ) @ spin_lat.sso('c', site ) )
+        h_onsite_r += ( eps_vector_r[ site - n_lead_left - n_system ] - mu_R ) * ( ferm_lat.sso('ch',site ) @ ferm_lat.sso('c', site ) )
     
     # system-lead coupling
     #every lead site is coupled to the rightmost system site
     h_sys_lead = np.zeros( (dim_tot, dim_tot), dtype = 'complex' )
     for site in range( n_lead_left + n_system , n_tot ):
         print( 'coupling {} and {}'.format( site, n_system + n_lead_left -1 ) )
-        h_sys_lead += k_vector_r[ site - n_lead_left - n_system ] *  ( spin_lat.sso('ch',site ) @ spin_lat.sso('c', n_system + n_lead_left -1 ) + spin_lat.sso('ch',n_system + n_lead_left -1 ) @ spin_lat.sso('c', site ) )
+        h_sys_lead += k_vector_r[ site - n_lead_left - n_system ] *  ( ferm_lat.sso('ch',site ) @ ferm_lat.sso('c', n_system + n_lead_left -1 ) + ferm_lat.sso('ch',n_system + n_lead_left -1 ) @ ferm_lat.sso('c', site ) )
     
         
     h_left_lead = h_onsite_r + h_sys_lead    
@@ -149,22 +149,22 @@ def H_leads_right( eps_vector_r, k_vector_r, mu_R ):
 def lindblad_op_list_left_lead( eps_delta_vector_l, eps_vector_l, mu_L, T_L ):
     l_list_left = []
     for site in range(n_lead_left):
-        l_list_left.append( np.sqrt( eps_delta_vector_l[site] * np.exp( 1./T_L * ( eps_vector_l[site] - mu_L ) ) * fermi_dist( 1./T_L, eps_vector_l[site], mu_L ) ) * spin_lat.sso( 'c',site ) )
-        l_list_left.append( np.sqrt( eps_delta_vector_l[site]* fermi_dist( 1./T_L, eps_vector_l[site], mu_L)) * spin_lat.sso('ch',site) )
+        l_list_left.append( np.sqrt( eps_delta_vector_l[site] * np.exp( 1./T_L * ( eps_vector_l[site] - mu_L ) ) * fermi_dist( 1./T_L, eps_vector_l[site], mu_L ) ) * ferm_lat.sso( 'c',site ) )
+        l_list_left.append( np.sqrt( eps_delta_vector_l[site]* fermi_dist( 1./T_L, eps_vector_l[site], mu_L)) * ferm_lat.sso('ch',site) )
     return l_list_left
 
 def lindblad_op_list_right_lead( eps_delta_vector_r, eps_vector_r, mu_R, T_R ):
     l_list_right = []
     for site in range(n_lead_left):
-        l_list_right.append( np.sqrt( eps_delta_vector_r[site] * np.exp( 1./T_R * ( eps_vector_r[site] - mu_R ) ) * fermi_dist( 1./T_R, eps_vector_r[site], mu_R ) ) * spin_lat.sso( 'c',site ) )
-        l_list_right.append( np.sqrt( eps_delta_vector_r[site]* fermi_dist( 1./T_R, eps_vector_r[site], mu_R) ) * spin_lat.sso('ch',site) )
+        l_list_right.append( np.sqrt( eps_delta_vector_r[site] * np.exp( 1./T_R * ( eps_vector_r[site] - mu_R ) ) * fermi_dist( 1./T_R, eps_vector_r[site], mu_R ) ) * ferm_lat.sso( 'c',site ) )
+        l_list_right.append( np.sqrt( eps_delta_vector_r[site]* fermi_dist( 1./T_R, eps_vector_r[site], mu_R) ) * ferm_lat.sso('ch',site) )
     return l_list_right     
 
 
 def H_sistem_t(A, om, dt, t_max, which_timestep):
     t_vec = np.arange(0,t_max,dt)
     eps = A * np.cos( om * t_vec[which_timestep] )
-    h_sys = eps * spin_lat.sso('ch', n_lead_left) @ spin_lat.sso('ch', n_lead_left)
+    h_sys = eps * ferm_lat.sso('ch', n_lead_left) @ ferm_lat.sso('ch', n_lead_left)
     return h_sys 
 
 
@@ -181,6 +181,6 @@ l_list_right = lindblad_op_list_right_lead( eps_delta_vector_r, eps_vector_r, mu
 ######
 
 #INITAL STATE: vacuum for leads and 1/sqrt(2) (|0> + |1>) for the system (single dot)
-init_state = spin_lat.vacuum_state #vacuum state = all up
+init_state = ferm_lat.vacuum_state #vacuum state = all up
 
 ##SOLVE LINDBLAD EQUATION
