c163f01e4cfd00e728ec418643afa8b995a0f4e9
Yudong Sun <yudong.sun@physik.uni-muenchen.de>
2024-04-24 09:57:21 +0200

    edit __init__.py files such that modules are auto imported

--- evos/src/lattice/__init__.py
+++ evos/src/lattice/__init__.py
@@ -1,1 +1,9 @@
-#from evos.src.lattice.lattice import *
+import pkgutil
+
+# Reference, How to import all submodules: https://stackoverflow.com/a/3365846/3211506
+
+__all__ = []
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    _module = loader.find_module(module_name).load_module(module_name)
+    globals()[module_name] = _module
