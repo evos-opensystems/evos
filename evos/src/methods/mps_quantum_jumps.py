cff9dbcf98fdc1890e8e316fa9884597758ba8f2
Mattia Moroder <ra69tuc@kcs-login.cos.lrz.de>
2024-05-07 11:18:07 +0200

    removed scipy import

--- evos/src/methods/mps_quantum_jumps.py
+++ evos/src/methods/mps_quantum_jumps.py
@@ -3,7 +3,6 @@ import os
 import sys
 import time
 from numpy import linalg as LA
-from scipy.linalg import expm
 import psutil
 import pyten as ptn
 ##import evos.src.observables.observables as observables
