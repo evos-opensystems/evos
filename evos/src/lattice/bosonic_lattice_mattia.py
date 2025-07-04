c573e61610c054740f250fdcde43cef4654179e9
Yudong Sun <yudong.sun@physik.uni-muenchen.de>
2025-07-04 10:23:29 +0200

    add get fock state to the ED lattice

--- evos/src/lattice/bosonic_lattice_mattia.py
+++ evos/src/lattice/bosonic_lattice_mattia.py
@@ -2,6 +2,7 @@
 
 First make this independently, in order to see what methods and attributes it need.
 Later make it inherit from an ABC class 'Lattice'. """
+from typing import List, Union
 import numpy as np
 
 
@@ -45,6 +46,40 @@ class BosonicLatticeMattia():
         vacuum_state = np.zeros( ( max_bosons + 1 )**n_sites, dtype='complex')
         vacuum_state[0] = 1
         self.vacuum_state = vacuum_state
+
+    def get_fock_state(self, occupations: Union[List[int], np.ndarray]) -> np.ndarray:
+        """Given occupations on each site, generate the occupation.
+
+        Function uses the python built-in `int(str, base)`, which is limited to 2-36 for the local dimension. 
+        See https://docs.python.org/3/library/functions.html#int
+
+        Args:
+            occupations (Union[List[int], np.ndarray]): 1D list of occupations corresponding to the site
+
+        Returns:
+            scipy.sparse.csr_matrix: Fock state
+        """
+
+        assert len(occupations) == self.n_sites, f"The number of sites in the input occupation list ({len(occupations) }) does not match the number of sites in the lattice ({self.n_sites})."
+        assert np.all([0 <= occupation <= self.max_bosons for occupation in occupations]), f"Occupations must be between 0 and max_bosons = {self.max_bosons}."
+
+        if self.max_bosons > 35:
+            raise NotImplementedError(f"Local dimension = {self.max_bosons + 1} is too large for the built-in int(str, base) function.")
+            # If this happens, and you need it, implement the conversion manually
+            # idx = sum_i(occupation_i * (max_bosons + 1)**i) from LSB to MSB
+
+        if self.max_bosons > 9:
+            # Since anything more than 9/site i.e. localdim = 10 is not representable by a single digit
+            # convert occupations to letters
+            occupations: List[Union[str, int]] = [chr((_occ-10) + 65) if _occ > 9 else _occ for _occ in occupations]
+        
+        _occ_str = "".join([str(occupation) for occupation in occupations])
+        _occ_idx = int(_occ_str, self.max_bosons + 1) # str in base (max_bosons + 1) to int
+
+        _state = np.zeros(self.vacuum_state.shape, dtype='complex')
+        _state[_occ_idx] = 1
+
+        return _state
     
     def sso(self, operator_name: str, site: int) -> np.ndarray :
         """Given an operator name and a site number, it computes the single site operator acting on the whole Hilbert space
